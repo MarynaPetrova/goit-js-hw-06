cb882b7b470634de36cd1ffccd9d9baeca181887
MarynaPetrova <marina.bitkova@gmail.com>
2023-09-08 18:50:24 +0200

    fix

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,5 @@ const fontSizeControl = document.getElementById("font-size-control");
 const textElement = document.getElementById("text");
 
 fontSizeControl.addEventListener("input", function () {
-  const fontSizeValue = fontSizeControl.value;
-
-  textElement.style.fontSize = fontSizeValue + "px";
+  textElement.style.fontSize = fontSizeControl.value + "px";
 });
